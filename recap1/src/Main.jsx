daf65b69e04cc9f16e30dc9d94e53968707c87de
Osman Ali Durna <o_durna@hotmail.com>
2024-10-15 20:47:54 +0200

    internal, external exercises

--- recap1/src/Main.jsx
+++ recap1/src/Main.jsx
@@ -1,5 +1,11 @@
 const Main = () => {
-  return <main>Hallo Main</main>;
+  const parStyle = {
+    fontFamily: "tahoma",
+    color: "#333",
+    fontSize: "1.5rem",
+  };
+
+  return <main style={parStyle}>Hallo Main</main>;
 };
 
 export default Main;
