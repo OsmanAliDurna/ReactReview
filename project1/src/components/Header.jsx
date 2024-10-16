ed568757d47422c673bb002809775e0177c04525
Osman Ali Durna <o_durna@hotmail.com>
2024-10-16 15:00:46 +0200

    scss examples completed

---
+++ project1/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import HeaderStyle from "../scss/header.module.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <h1 className={HeaderStyle.header}>COMMENTS</h1>
+    </header>
+  );
+};
+
+export default Header;
